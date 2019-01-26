c02ef6baa5146f7df0d66d3832d285e93208f8f6
Nisan Coskun <dev@alpcoskun.com>
2019-01-26 23:44:13 +0300

    JSON.stringify -> fast-safe-stringify, sha1 -> md5 keys

--- src/generate-key.js
+++ src/generate-key.js
@@ -1,13 +1,28 @@
 'use strict';
 
-const jsosort = require('jsosort');
-const sha1 = require('sha1');
+const { createHash } = require('crypto');
+const stringify = require('fast-safe-stringify');
+
+const jsosort = function(obj) {
+  let result = {};
+  Object.keys(obj)
+    .sort()
+    .forEach(function(key) {
+      let value = obj[key];
+      if (Object.prototype.toString.call(value) === '[object Object]') {
+        value = jsosort(value);
+      }
+      result[key] = value;
+    });
+  return result;
+};
+
+const replacer = function(key, val) {
+  return val instanceof RegExp ? String(val) : val;
+};
 
 module.exports = function init(obj) {
-  obj = jsosort(obj);
-  obj = JSON.stringify(obj, (key, val) => {
-    return val instanceof RegExp ? String(val) : val;
-  });
-
-  return sha1(obj);
+  return createHash('md5')
+    .update(stringify(jsosort(obj), replacer))
+    .digest('hex');
 };
